94a92eb29f2d2aa918ad7f554b518bc768a1969a
tritonrigel <67826350+tritonrigel@users.noreply.github.com>
2020-07-05 15:49:43 -0700

    First commit

---
+++ android/app/src/main/kotlin/com/example/librebook/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.triton.librebook
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Librebook',
+    );
+  }
+}
