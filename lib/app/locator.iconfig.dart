2a55095959d334ae4421bb833b73684cf59bed7f
atticdev <67826350+atticdev@users.noreply.github.com>
2020-07-12 15:15:34 +0700

    Change book model and delete author model

--- lib/app/locator.iconfig.dart
+++ lib/app/locator.iconfig.dart
@@ -4,12 +4,14 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
+import 'package:librebook/services/book_service.dart';
 import 'package:librebook/services/third_party_services_module.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:get_it/get_it.dart';
 
 void $initGetIt(GetIt g, {String environment}) {
   final thirdPartyServicesModule = _$ThirdPartyServicesModule();
+  g.registerLazySingleton<BookService>(() => BookService());
   g.registerLazySingleton<DialogService>(
       () => thirdPartyServicesModule.dialogService);
   g.registerLazySingleton<NavigationService>(
