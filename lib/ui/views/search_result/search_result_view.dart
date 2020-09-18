a038cf7a6bd845528984e647d20e9c4ac2e5de87
tritonrigel <67826350+tritonrigel@users.noreply.github.com>
2020-09-18 15:15:47 +0700

    Completed search result view for general & fiction books

--- lib/ui/views/search_result/search_result_view.dart
+++ lib/ui/views/search_result/search_result_view.dart
@@ -1,8 +1,13 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lazy_load_refresh_indicator/lazy_load_refresh_indicator.dart';
 import 'package:librebook/controllers/search_result_controller.dart';
 import 'package:librebook/models/book_search_detail_model.dart';
+import 'package:librebook/ui/shared/theme.dart';
+import 'package:librebook/ui/shared/ui_helper.dart';
+import 'package:librebook/ui/widgets/image_error_widget.dart';
+import 'package:shimmer/shimmer.dart';
 
 class SearchResultView extends StatefulWidget {
   final String query;
@@ -46,22 +51,119 @@ class _SearchResultViewState extends State<SearchResultView> {
         () => LazyLoadRefreshIndicator(
           child: ListView.builder(
             itemBuilder: (context, index) {
+              final book = controller.listBook[index];
               final isNextPage = index + 1 >= controller.listBook.length &&
                   !controller.isLastPage();
               if (isNextPage) {
                 return Padding(
-                  padding: EdgeInsets.only(bottom: 8),
+                  padding: EdgeInsets.only(bottom: 8, top: 16),
                   child: Column(
                     children: [CircularProgressIndicator()],
                   ),
                 );
               }
 
-              return ListTile(
-                title: Text(controller.listBook[index].title),
-                subtitle: Text(controller.listBook[index].authors.join(', ')),
-                trailing: Text(
-                  controller.listBook[index].id,
+              return Padding(
+                padding: EdgeInsets.only(left: 8, right: 8, top: 8),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: Get.height / 6,
+                      width: Get.height / 8,
+                      child: Stack(
+                        fit: StackFit.expand,
+                        children: [
+                          Positioned(
+                            top: 2,
+                            left: 2,
+                            child: Material(
+                              elevation: 3,
+                              color: primaryColor,
+                              child: Padding(
+                                  padding: EdgeInsets.all(8),
+                                  child: Text(
+                                    book.format,
+                                    style: TextStyle(
+                                        color: secondaryColor,
+                                        fontWeight: FontWeight.w600),
+                                  )),
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey[300])),
+                            child: CachedNetworkImage(
+                              imageUrl: book.cover,
+                              placeholder: (context, url) => Shimmer.fromColors(
+                                baseColor: Colors.grey[300],
+                                highlightColor: Colors.grey[100],
+                                child: Container(
+                                  width: double.infinity,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              fit: BoxFit.fill,
+                              errorWidget: (context, _, __) {
+                                return ImageErrorWidget();
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    horizontalSpaceSmall,
+                    Expanded(
+                      child: Container(
+                        height: Get.height / 6,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                verticalSpaceSmall,
+                                Text(
+                                  book.title,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                verticalSpaceSmall,
+                                Text(
+                                  book.authors.join(', '),
+                                  maxLines: 1,
+                                  style: TextStyle(fontSize: 14),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Format: ' + book.format,
+                                  style: TextStyle(
+                                    color: Colors.grey[600],
+                                  ),
+                                ),
+                                Text(
+                                  'Language: ' + book.language,
+                                  style: TextStyle(
+                                    color: Colors.grey[600],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               );
             },
