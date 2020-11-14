16de313500a05b93578e6a0f02281548d3191689
Bagas Wastu <67826350+tritonrigel@users.noreply.github.com>
2020-11-14 20:41:03 +0700

    Revert multi download

--- lib/controllers/download_controller.dart
+++ lib/controllers/download_controller.dart
@@ -1,61 +1,43 @@
-import 'package:dio/dio.dart';
 import 'package:downloads_path_provider_28/downloads_path_provider_28.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:get/get.dart';
 import 'package:get/state_manager.dart';
 import 'package:librebook/app/locator.dart';
+import 'package:librebook/database/download_database.dart';
 import 'package:librebook/models/book_model.dart';
 import 'package:librebook/services/download_service.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:path/path.dart' as path;
 
 class DownloadController extends GetxController {
   final _downloadServices = locator<DownloadService>();
-  RxBool isDownloading = false.obs;
-  RxString progress = '0'.obs;
-  Dio _dio;
+  final _downloadDb = DownloadDatabase();
 
-  DownloadController() : _dio = Dio();
+  ///is page currently download?
+  Future<bool> isCurrentlyDownload(String taskId)async {
+    final download = await _downloadDb.getByTaskId(taskId);
+    print('download: ' + download.toString());
+    return download != null;
+  }  
 
-  Future download(Book book) async {
+  /// return task id
+  Future<String> download(Book book) async {
     final status = await Permission.storage.request();
 
     if (status.isGranted) {
       final externalDir = await DownloadsPathProvider.downloadsDirectory;
 
-      // is Downloading
-      isDownloading.value = true;
-
       final isUrlWorking = await _downloadServices.checkMirror(book.mirrorUrl);
 
       if (!isUrlWorking) {
         print('error: url is not working');
-        return;
+        throw Exception('Url is not working');
       }
 
       final bookUrl = await _downloadServices.getDownloadUrl(book.mirrorUrl);
 
-      _dio.download(
-        bookUrl,
-        path.join(externalDir.path, book.title + ' - ' + book.authors[0]),
-        
-        onReceiveProgress: (rcv, total){
-          print('received: ${rcv.toStringAsFixed(0)} out of total: ${total.toStringAsFixed(0)}');
+      final taskId = await FlutterDownloader.enqueue(url: bookUrl, savedDir: externalDir.path);
 
-          progress.value = ((rcv / total) * 100).toStringAsFixed(0);
-
-          // isDownloading
-          if(progress.value == '100'){
-            isDownloading.value = false;
-          }else if(double.parse(progress.value) < 100){
-            isDownloading.value = true;
-          }
-        }
-      ).then((_) {
-        if(progress.value == '100'){
-          isDownloading.value = false;
-        }
-      });
-
+      return taskId;
     } else {
       print('permission denied');
     }
