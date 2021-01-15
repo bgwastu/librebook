a71162e725762fb6fc8242d19356f81b80998c42
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2021-01-15 20:50:52 +0700

    Add splash screen

--- lib/ui/views/splash/splash_view.dart
+++ lib/ui/views/splash/splash_view.dart
@@ -1,16 +1,29 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:librebook/controllers/splash_controller.dart';
 
 class SplashView extends StatelessWidget {
-
   final controller = Get.put(SplashController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Librebook'),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Center(
+            child: Container(
+              height: 150,
+              width: 150,
+              child: Image.asset('assets/icon/icon.png'),
+            ),
+          ),
+          Positioned(
+            bottom: 16,
+            child: Text('Librebook v0.0.1-alpha', style: TextStyle(color: Colors.grey[600]),),
+          )
+        ],
       ),
     );
   }
