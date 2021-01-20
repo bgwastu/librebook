e7fb123076f2508250ecda4047460e11086af019
atticdev <67826350+atticdev@users.noreply.github.com>
2021-01-20 15:49:23 +0700

    Add download location feature

--- lib/ui/views/home/settings_view.dart
+++ lib/ui/views/home/settings_view.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:librebook/controllers/settings_controller.dart';
+import 'package:librebook/ui/shared/theme.dart';
 import 'package:librebook/ui/shared/ui_helper.dart';
+import 'package:librebook/ui/widgets/folder_picker/folder_picker.dart';
+import 'package:librebook/ui/widgets/folder_picker/picker_common.dart';
 import 'package:outline_material_icons/outline_material_icons.dart';
 
 class SettingView extends StatelessWidget {
@@ -12,52 +17,61 @@ class SettingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      padding: EdgeInsets.only(top: 0, left: 8, right: 8),
+      padding: EdgeInsets.only(top: 8 , left: 8, right: 8),
       children: [
-        ListTile(
-          title: Text('Download location'),
-          subtitle: FutureBuilder(
-            future: _settingsController.getDownloadLocation(),
-            builder: (_, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data);
-              } else {
-                return Container();
-              }
-            },
-          ),
-          leading: Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: Icon(
-              OMIcons.saveAlt,
-              color: Colors.grey[700],
-              size: 26,
-            ),
-          ),
-          onTap: () async {
-            // Directory newDirectory = await DirectoryPicker.pick(
-            //     context: context,
-            //     rootDirectory: await defaultDownloadLocation
-            // );
-            //
-            // if (newDirectory != null) {
-            //   // Do something with the picked directory
-            //   print(newDirectory.path);
-            //
-            // } else {
-            //   // User cancelled without picking any directory
-            // }
-            //TODO; implement download location
-            Get.rawSnackbar(
-                title: 'Under development',
-                message: 'This feature is still under development',
-                icon: Icon(
-                  Icons.warning,
-                  color: Colors.white,
+        Theme(
+          data: ThemeData(primaryColor: secondaryColor),
+          child: ListTile(
+              title: Text('Download location'),
+              subtitle: FutureBuilder(
+                future: _settingsController.getDownloadLocation(),
+                builder: (_, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data);
+                  } else {
+                    return Container();
+                  }
+                },
+              ),
+              leading: Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Icon(
+                  OMIcons.saveAlt,
+                  color: Colors.grey[700],
+                  size: 26,
                 ),
-                animationDuration: Duration(milliseconds: 500),
-                duration: Duration(milliseconds: 1500));
-          },
+              ),
+              onTap: () async {
+                var newDirectory = await FilesystemPicker.open(
+                  context: context,
+                  rootDirectory: Directory('/storage/emulated/0'),
+                  fsType: FilesystemType.folder,
+                  title: 'Download Location',
+                  rootName: 'Internal Storage',
+                  pickText: 'Save book to this folder',
+                  folderIconColor: primaryColor,
+                );
+
+                print(newDirectory);
+
+                if (newDirectory != null) {
+                  // Do something with the picked directory
+                  _settingsController.setDownloadLocation(newDirectory);
+
+                }
+
+                //TODO: implement download location (DOING)
+                //   Get.rawSnackbar(
+                //       title: 'Under development',
+                //       message: 'This feature is still under development',
+                //       icon: Icon(
+                //         Icons.warning,
+                //         color: Colors.white,
+                //       ),
+                //       animationDuration: Duration(milliseconds: 500),
+                //       duration: Duration(milliseconds: 1500));
+                // },
+              }),
         ),
         Divider(
           height: 2,
